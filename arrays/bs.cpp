1d7befbfd99f48f156ead02e5bfb0bbf424f4781
Prashant-thakur77 <prashant101007@gmail.com>
2025-08-25 14:12:19 +0530

    done

--- arrays/bs.cpp
+++ arrays/bs.cpp
@@ -1,64 +1,41 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-#define ll long long
-#define fast_io ios::sync_with_stdio(false); cin.tie(nullptr)
-#define all(x) (x).begin(), (x).end()
-#define pb push_back
-#define mod 1000000007
+int main() {
+    cout << "ROLL NO: 24BCS085\n";
+    int t; 
+    cout << "Enter number of test cases: ";
+    cin >> t;
+    while (t--) {
+        int n, x;
+        cout << "Enter size of array: ";
+        cin >> n;
+        vector<int> v(n);
+        cout << "Enter elements: ";
+        for (int &i : v) cin >> i;
+        sort(v.begin(), v.end());
+        cout << "Enter number to search: ";
+        cin >> x;
 
-int32_t main() {
-  int no;
-  cout<<"Enter no to search :";
-  cin>>no;
-  int n;
-  cout<<"Enter size of the array :";
-  cin>>n;
+        int low = 0, high = n - 1, found = -1;
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            if (v[mid] == x) { found = mid; break; }
+            else if (v[mid] < x) low = mid + 1;
+            else high = mid - 1;
+        }
 
-  vector<int> v(n);
-  cout<<"Enter elements of the array :";
-  for(int i=0;i<n;i++){
-    
-    cin>>v[i];
-  }
-  /*int last=v[0];
-  for(int i=1;i<n;i++){
-    if(v[i]<last){
-      cout<<-1<<endl;
-      return 0;
-    }
-    else last=v[i];
-  }*/
-  sort(all(v));
-  
+        if (found != -1) {
+            cout << "Number found at index " << found << endl;
+            v.erase(v.begin() + found);
+        } else {
+            cout << "Number not found. Inserting in sorted order.\n";
+            v.insert(v.begin() + low, x);
+        }
 
-  int low=0;
-  int high=n-1;
-  int flag=0;
-  while(low<=high){
-    int mid=(low+high)/2;
-    int noatmid=v[mid];
-    if(noatmid==no){
-      cout<<"No found at index "<<mid<<endl;
-      v.erase(v.begin()+mid);
-      flag=1;
-      break;
+        cout << "Updated array: ";
+        for (int i : v) cout << i << " ";
+        cout << "\n";
     }
-    else if(noatmid<no){
-      low=mid+1;
-    }
-    else high=mid-1;
-  }
-  //cout<<high<<" "<<low<<endl;
-  if(flag==0){
-    cout<<"No was not present int he array hence inserting the no in sorted manner "<<endl;
-    v.insert(v.begin()+low,no);
-  }
-  cout<<"New array:";
-  for(int i=0;i<v.size();i++){
-    cout<<v[i]<<" ";
-  }
-  cout<<endl;
-  return 0;
-  
+    return 0;
 }
--- arrays/bubble.cpp
+++ arrays/bubble.cpp
@@ -1,36 +1,35 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-#define ll long long
-#define fast_io ios::sync_with_stdio(false); cin.tie(nullptr)
-#define all(x) (x).begin(), (x).end()
-#define pb push_back
-#define mod 1000000007
-
-
-
 int32_t main() {
-    ll n;
-    cin>>n;
-    vector<int> v(n);
-    for(int i=0;i<n;i++){
-      cin>>v[i];
-    }
-    for(int i=0;i<n-1;i++){
-      bool flag=true;
-      for(int j=0;j<n-i-1;j++){
-        if(v[j]>v[j+1]){
-          swap(v[j],v[j+1]);
-          flag=false;
+    cout<<"ROLL NO: 24BCS085"<<endl;
+    int t;
+    cout<<"Enter number of test cases: ";
+    cin>>t;
+    while(t--){
+        long long n;
+        cout<<"Enter number of elements: ";
+        cin>>n;
+        vector<int> v(n);
+        cout<<"Enter elements: ";
+        for(int i=0;i<n;i++){
+            cin>>v[i];
         }
-      }
-      if(flag){
-        break;
-      }
+        for(int i=0;i<n-1;i++){
+            bool flag=true;
+            for(int j=0;j<n-i-1;j++){
+                if(v[j]>v[j+1]){
+                    swap(v[j],v[j+1]);
+                    flag=false;
+                }
+            }
+            if(flag) break;
+        }
+        cout<<"Sorted elements: ";
+        for(int i=0;i<n;i++){
+            cout<<v[i]<<" ";
+        }
+        cout<<endl;
     }
-    for(int i=0;i<n;i++){
-      cout<<v[i]<<" ";
-    }
-    cout<<endl;
     return 0;
 }
